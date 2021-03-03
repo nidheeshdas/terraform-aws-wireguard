64dc00e155286f83b782161d73a9f316904c3090
nidheeshdas <nidheeshdas@gmail.com>
2021-03-03 19:38:27 +0530

    updated tasks

--- main.tf
+++ main.tf
@@ -3,21 +3,21 @@ data "template_file" "user_data" {
 
   vars = {
     wg_server_private_key = data.aws_ssm_parameter.wg_server_private_key.value
-    wg_server_net         = var.wg_server_net
-    wg_server_port        = var.wg_server_port
-    peers                 = join("\n", data.template_file.wg_client_data_json.*.rendered)
-    eip_id                = var.eip_id
-    wg_server_interface   = var.wg_server_interface
+    wg_server_net = var.wg_server_net
+    wg_server_port = var.wg_server_port
+    peers = join("\n", data.template_file.wg_client_data_json.*.rendered)
+    eip_id = var.eip_id
+    wg_server_interface = var.wg_server_interface
   }
 }
 
 data "template_file" "wg_client_data_json" {
   template = file("${path.module}/templates/client-data.tpl")
-  count    = length(var.wg_client_public_keys)
+  count = length(var.wg_client_public_keys)
 
   vars = {
-    client_pub_key       = element(values(var.wg_client_public_keys[count.index]), 0)
-    client_ip            = element(keys(var.wg_client_public_keys[count.index]), 0)
+    client_pub_key = element(values(var.wg_client_public_keys[count.index]), 0)
+    client_ip = element(keys(var.wg_client_public_keys[count.index]), 0)
     persistent_keepalive = var.wg_persistent_keepalive
   }
 }
@@ -30,15 +30,19 @@ data "aws_ami" "ubuntu" {
     values = ["ubuntu/images/hvm-ssd/ubuntu-*-16.04-amd64-server-*"]
   }
   filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+    name = "virtualization-type"
+    values = [
+      "hvm"]
   }
-  owners = ["099720109477"] # Canonical
+  owners = [
+    "099720109477"]
+  # Canonical
 }
 
 # turn the sg into a sorted list of string
 locals {
-  sg_wireguard_external = sort([aws_security_group.sg_wireguard_external.id])
+  sg_wireguard_external = sort([
+    aws_security_group.sg_wireguard_external.id])
 }
 
 # clean up and concat the above wireguard default sg with the additional_security_group_ids
@@ -47,14 +51,16 @@ locals {
 }
 
 resource "aws_launch_configuration" "wireguard_launch_config" {
-  name_prefix                 = "wireguard-${var.env}-"
-  image_id                    = var.ami_id == null ? data.aws_ami.ubuntu.id : var.ami_id
-  instance_type               = var.instance_type
-  key_name                    = var.ssh_key_id
-  iam_instance_profile        = (var.eip_id != "disabled" ? aws_iam_instance_profile.wireguard_profile[0].name : null)
-  user_data                   = data.template_file.user_data.rendered
-  security_groups             = local.security_groups_ids
-  associate_public_ip_address = (var.eip_id != "disabled" ? true : false)
+  name_prefix = "wireguard-${var.env}-"
+  image_id = var.ami_id == null ? data.aws_ami.ubuntu.id : var.ami_id
+  instance_type = var.instance_type
+  spot_price = "0.01"
+  key_name = var.ssh_key_id
+  iam_instance_profile = (var.eip_id != "disabled" ? aws_iam_instance_profile.wireguard_profile[0].name : null)
+  user_data = data.template_file.user_data.rendered
+  security_groups = local.security_groups_ids
+  //  associate_public_ip_address = (var.eip_id != "disabled" ? true : false)
+  associate_public_ip_address = true
 
   lifecycle {
     create_before_destroy = true
@@ -62,15 +68,17 @@ resource "aws_launch_configuration" "wireguard_launch_config" {
 }
 
 resource "aws_autoscaling_group" "wireguard_asg" {
-  name                 = aws_launch_configuration.wireguard_launch_config.name
+  name = aws_launch_configuration.wireguard_launch_config.name
   launch_configuration = aws_launch_configuration.wireguard_launch_config.name
-  min_size             = var.asg_min_size
-  desired_capacity     = var.asg_desired_capacity
-  max_size             = var.asg_max_size
-  vpc_zone_identifier  = var.subnet_ids
-  health_check_type    = "EC2"
-  termination_policies = ["OldestLaunchConfiguration", "OldestInstance"]
-  target_group_arns    = var.target_group_arns
+  min_size = var.asg_min_size
+  desired_capacity = var.asg_desired_capacity
+  max_size = var.asg_max_size
+  vpc_zone_identifier = var.subnet_ids
+  health_check_type = "EC2"
+  termination_policies = [
+    "OldestLaunchConfiguration",
+    "OldestInstance"]
+  target_group_arns = var.target_group_arns
 
   lifecycle {
     create_before_destroy = true
@@ -78,23 +86,23 @@ resource "aws_autoscaling_group" "wireguard_asg" {
 
   tags = [
     {
-      key                 = "Name"
-      value               = aws_launch_configuration.wireguard_launch_config.name
+      key = "Name"
+      value = aws_launch_configuration.wireguard_launch_config.name
       propagate_at_launch = true
     },
     {
-      key                 = "Project"
-      value               = "wireguard"
+      key = "Project"
+      value = "wireguard"
       propagate_at_launch = true
     },
     {
-      key                 = "env"
-      value               = var.env
+      key = "env"
+      value = var.env
       propagate_at_launch = true
     },
     {
-      key                 = "tf-managed"
-      value               = "True"
+      key = "tf-managed"
+      value = "True"
       propagate_at_launch = true
     },
   ]
